d5ab324a4bc3821b5877a1f811177a819f7104b6
Athul preet <preetathul@gmail.com>
2018-12-25 21:25:30 +0530

    variables

---
+++ variable.py
@@ -0,0 +1,19 @@
+age = 14
+age
+varName = 12
+varName = "lol"
+person1, person2, person3 = "abby", "james", "lebron"
+print(person1)
+print(person2)
+print(person3)
+var1 = var2 = var3 = "apples"
+print(var1)
+print(var2)
+print(var3)
+age = 14
+age2 = 20
+age3 = age + age2
+print(age3)
+random = "athul preet"
+print(random[3:3])
+
